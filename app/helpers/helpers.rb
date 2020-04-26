6774d35c9790e874f40ed429df28de50c018bdab
Megan Horsfield <meganthadev@gmail.com>
2020-04-26 08:32:23 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,11 @@
 class Helpers
   
   def current_user(session)
-   @current_user = User.find_by_id(session[:user_id])
+   @user = User.find_by_id(session[:user_id])
   end 
   
-  def is_logged_in?
+  def is_logged_in?(session)
+    
     
   end 
   
