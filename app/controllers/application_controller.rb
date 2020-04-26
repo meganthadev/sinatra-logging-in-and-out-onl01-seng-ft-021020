7fff4a2401f1c5ec9e9ac6d9d93a66c61a2ddbb3
Megan Horsfield <meganthadev@gmail.com>
2020-04-26 08:22:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/account'
     else 
-      redirect ''
+      erb :error 
+    end 
   end
 
   get '/account' do
