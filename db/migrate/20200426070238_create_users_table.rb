a3f26f647420fa65fb00c2412a8342e23b04f688
Megan Horsfield <meganthadev@gmail.com>
2020-04-26 08:02:33 +0000

    Automatically backed up by Learn

--- db/migrate/20200426070238_create_users_table.rb
+++ db/migrate/20200426070238_create_users_table.rb
@@ -3,6 +3,8 @@ class CreateUsersTable < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username 
       t.string :password 
-      t.integer :balance
+      t.decimal :balance
+      t.timestamps
+    end
   end
 end
